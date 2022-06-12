c55f082e8da2c055671bcc07fc37b77338ca4b3f
JurateCepaitiene <jurate.cepaitiene@gmail.com>
2022-06-12 20:22:46 +0300

    ID code verifier done

---
+++ repeticijos/rep1.py
@@ -0,0 +1,44 @@
+# a = 5
+
+# zodis = "Labas"
+# dar_vienas = "Šitas žodis"
+
+# print("a lygu: " + str(a) + ", žodis: " + zodis + ", dar vienas žodis – " + dar_vienas)
+
+# # Geresnis variantas:
+# print(f"a lygu {a}, žodis: {zodis}, dar vienas žodis – {dar_vienas}")
+
+# # kas cia?
+
+# test = """
+# Sveiki, draugai,
+# Ši pastraipa nebus vykdoma.
+# """
+
+# print(test)
+
+# test2 = """Sveiki, draugai,Ši pastraipa nebus vykdoma"""
+
+# print(test2)
+
+# naujas_zodis = "Code Academy"
+
+# print(naujas_zodis[11:4:-1])
+# print(naujas_zodis[:-1])
+
+# def daugiau_nei_2(list):
+#     sarasas_2 = []
+#     for n in list:
+#         if n > 2:
+#             sarasas_2.append(n)
+#     return sarasas_2
+
+# sarasas = [4, 3, 2, 1]
+
+# print(daugiau_nei_2(sarasas))
+
+# naujas_sarasas = [5, 7, 10, -1, -4, 0]
+# print(sarasas)
+# print(daugiau_nei_2(naujas_sarasas))
+
+
